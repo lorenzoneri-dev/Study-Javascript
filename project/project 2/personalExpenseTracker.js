f17ba49471519980ddfc7fcaa4d01a4cccf3080b
Lorenzo Neri <nerilorenzo412@gmail.com>
2026-09-14 09:53:13 +0200

    add input, fix array error, add input verify, add output

--- project/project 2/personalExpenseTracker.js
+++ project/project 2/personalExpenseTracker.js
@@ -1,10 +1,41 @@
-let spesa = {
-    nome: [],
-    importo: [],
-    categoria: [],
-    data: [],
-};
+let spesa = [];
 
-function aggiungiSpesa() {
+async function aggiungiSpesa(spesa) {
+    const readline = require("readline/promises");
 
+    const input = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    const nome = await input.question("Write the name of the product: ");
+
+    let importo;
+    while(true){
+        const importoInput = await input.question("Write the price of the product: ");
+        importo = Number(importoInput.trim());
+
+        if(Number.isFinite(importo) && importo > 0){
+            break;
+        }
+
+        console.log("You must write a positive number.")
+    };
+
+    const categoria = await input.question("Write the category of the product:  ")
+    const data = await input.question("Write the date of the day: ");
+
+    const nuovaSpesa = {
+        nome,
+        importo,
+        categoria,
+        data
+    };
+
+    spesa.push(nuovaSpesa);
+    input.close();
 }
+
+aggiungiSpesa(spesa).then(() => {
+    console.log(spesa);
+});
